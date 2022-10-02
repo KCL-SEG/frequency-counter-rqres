620aa799a094fbdbeccaaf72bfaa1087986329f2
Rares Tamasanu <rares.ioan@icloud.com>
2022-10-02 23:43:27 +0100

    Solve assignment

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    # Your code goes HERE
+    items = [str(i) for i in items]
+    myset = set(items)
+    
+    for item in myset:
+        freq = items.count(item)
+        frequencies[str(item)] = freq
+
     return frequencies
+    
